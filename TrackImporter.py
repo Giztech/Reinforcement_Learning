2f6b6760724925e2dfa3a16f6ae8effb6d4f0db7
Kieran <ringel.kieran@gmail.com>
2021-11-30 15:20:06 -0700

    read in file

--- TrackImporter.py
+++ TrackImporter.py
@@ -1,7 +1,27 @@
 
 class TrackImporter:
-    def __init__(self):
-        pass
+    def __init__(self, file):
+        self.file_name = file
 
-    def importTrack(self, file):
-        return track
+    def importTrack(self):
+        file = open(self.file_name, 'r')  # opens file
+        fileline = file.read().split('\n') #split on line
+        startlist = []
+        track = []
+        fileline = fileline[1:]
+        for i in range(len(fileline)):
+            row = []
+            print(fileline[i])
+            if not fileline[i]:
+                continue
+            for j in range(len(fileline[i])):
+                if fileline[i][j] == 'S':
+                   startlist.append([i, j])
+                   row.append(-1)
+                elif fileline[i][j] == '#':
+                    row.append(None)
+                elif fileline[i][j] == '.':
+                    row.append(-1)
+                elif fileline[i][j] == 'F':
+                    row.append(1)
+            track.append(row)
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from TrackImporter import TrackImporter
 from Simulator import Simulator
 
 def main():
-    pass
+    ti = TrackImporter("data/L-track.txt")
+    track = ti.importTrack()
 
 if __name__ == '__main__':
     main()
