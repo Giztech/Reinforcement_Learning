fc615b81710e8f0151e6db83ced3520f247eb163
Mason Medina <masonmedina29@gmail.com>
2021-12-01 10:51:14 -0700

    updates

--- ValueIteration.py
+++ ValueIteration.py
@@ -5,21 +5,31 @@ class ValueIteration:
     def __init__(self):
         pass
 
-    def valueIteration(self, MDP, epislon):
+    def valueIteration(self, MDP, epsilon = 0.01):
 
 
+        #AIMA CODE
+        '''
+         U1 = {s: 0 for s in MDP.states}
+        R, T, discount = MDP.R, MDP.T, MDP.discount
+        while True:
+            U = U1.copy()
+            delta = 0
+            for s in MDP.states:
+                U1[s] = R(s) + discount * max(sum(p * U[s1] for (p, s1) in T(s,a)) for a in MDP.actions(s))
+                delta = max(delta, abs(U1[s]-U[s]))
+            if delta <= epsilon * (1-discount) / discount:
+                return U
+        '''
 
-
-
-        return 0
 
     def maximizePolicy(self, MDP, U):
 
         pi = {}
         for s in MDP.states:
-            pi[s] = max(MDP.actions(s), key=lambda a: self.utility(a,s,U,MDP))
+            pi[s] = max(MDP.actions(s), key=lambda a: self.utility(a, s, U, MDP))
         return pi
 
     def utility(self, a, s, U, MDP):
 
-        return sum(p * U[s1] for (p,s1) in MDP.T(s,a))
+        return sum(p * U[s1] for (p, s1) in MDP.T(s, a))
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from TrackImporter import TrackImporter
 from Simulator import Simulator
 from MDP import MDP
+from ValueIteration import ValueIteration
 
 def main():
     ti = TrackImporter("data/L-track.txt")
     track, start, size = ti.importTrack()
     mdp = MDP(size)
     sim = Simulator(track, start, [0,0])
+
+    vi = ValueIteration
+    vi.maximizePolicy(sim, vi.valueIteration(MDP,mdp))
 
 if __name__ == '__main__':
     main()
