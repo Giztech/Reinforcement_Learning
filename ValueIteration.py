a9f9e03e735765bbfc77269193829754c5b58c8c
Mason Medina <masonmedina29@gmail.com>
2021-12-01 10:33:22 -0700

    value iteration

--- ValueIteration.py
+++ ValueIteration.py
@@ -6,10 +6,20 @@ class ValueIteration:
         pass
 
     def valueIteration(self, MDP, epislon):
-        return U
+
+
+
+
+
+        return 0
 
     def maximizePolicy(self, MDP, U):
+
+        pi = {}
+        for s in MDP.states:
+            pi[s] = max(MDP.actions(s), key=lambda a: self.utility(a,s,U,MDP))
         return pi
 
     def utility(self, a, s, U, MDP):
-        return sum
+
+        return sum(p * U[s1] for (p,s1) in MDP.T(s,a))
