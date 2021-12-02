1f3f916a0ffb8c889704204688d0bb4003446d12
Mason Medina <masonmedina29@gmail.com>
2021-12-02 15:24:42 -0700

    merging

--- TrackImporter.py
+++ TrackImporter.py
@@ -2,8 +2,7 @@
 class TrackImporter:
     def __init__(self, file):
         self.file_name = file
-        self.start = self.findStartCells()
-        self.finish = self.findFinishCells()
+
 
 
     def importTrack(self):
@@ -30,26 +29,5 @@ class TrackImporter:
             track.append(row)
         return track, startlist, size
 
-    def isWall(self, cell: tuple):
-        x = cell[0]
-        y = cell[1]
-        if self.track[y][x] == '#' or x > len(self.track[0]) or x < 0 or y > len(self.track) or y < 0:
-            return True
-        else:
-            return False
 
-    def findStartCells(self):
-        startCells = []
-        for y in range(len(self.track)):
-            for x in range(len(self.track[0])):
-                if self.track[y][x] == 'S':
-                    startCells.append((x, y))
-        return startCells
 
-    def findFinishCells(self):
-        finishCells = []
-        for y in range(len(self.track)):
-            for x in range(len(self.track[0])):
-                if self.track[y][x] == 'F':
-                    finishCells.append((x, y))
-        return finishCells
